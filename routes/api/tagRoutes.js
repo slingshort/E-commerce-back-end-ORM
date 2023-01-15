8e440c26386a096f3f17c4c0d29342b67a271622
slingshort <salina.myat@gmail.com>
2023-01-16 01:47:30 +1100

    changed .env based on starter code, debugged tagRoutes

--- routes/api/tagRoutes.js
+++ routes/api/tagRoutes.js
@@ -4,7 +4,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 // GET req to get all tags
 router.get('/', async (req, res) => {
     try {
-      const tagData = await tagData.findAll();
+      const tagData = await Tag.findAll();
       res.status(200).json(tagData);
     } catch (err) {
       res.status(500).json(err);
